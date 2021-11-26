395add1e1479c9b030a2d951b71dd3262882ea47
eduardo4394 <eduardo_sh@outlook.com>
2021-11-26 09:54:57 -0500

    rubocop offenses solved

--- game.rb
+++ game.rb
@@ -2,9 +2,8 @@
 require_relative "pokedex/pokemons"
 require_relative "welcome_messages"
 
-include Messages
-
 class Game
+  include Messages
   def initialize
     @name = ""
     @pokemon = ""
@@ -26,42 +25,24 @@ class Game
     puts "people, POKEMON are pets. Others use them for fights. Myself..."
     puts "I study POKEMON as a profession."
     # Then create a Player with that information and store it in @player
+
     # Suggested game flow
     # action = menu
-    #   until action == "Exit"
-    #     case action
-    #     when "Train"
-    #       train
-    #       action = menu
-    #     when "Leader"
-    #       challenge_leader
-    #       action = menu
-    #     when "Stats"
-    #       show_stats
-    #       action = menu
-    #     end
+    # until action == "Exit"
+    #   case action
+    #   when "Train"
+    #     train
+    #     action = menu
+    #   when "Leader"
+    #     challenge_leader
+    #     action = menu
+    #   when "Stats"
+    #     show_stats
+    #     action = menu
     #   end
-    #   goodbye
-    # def train
-    #   # Complete this
-    # end
-
-    # def challenge_leader
-    #   # Complete this
-    # end
-
-    # def show_stats
-    #   # Complete this
-    # end
-
-    # def goodbye
-    #   # Complete this
-    # end
-
-    # def menu
-    #   # Complete this
     # end
   end
+
   # Create a welcome method(s) to get the name, pokemon and pokemon_name from the user
   def welcome
     puts "First, what is your name?"
@@ -84,7 +65,26 @@ class Game
     print "> "
     @pokemon_name = gets.chomp
     @pokemon_name.empty? && (@pokemon_name = @pokemon)
-    p @pokemon_name.capitalize
+  end
+
+  def train
+    # Complete this
+  end
+
+  def challenge_leader
+    # Complete this
+  end
+
+  def show_stats
+    # Complete this
+  end
+
+  def goodbye
+    # Complete this
+  end
+
+  def menu
+    # Complete this
   end
 end
 
